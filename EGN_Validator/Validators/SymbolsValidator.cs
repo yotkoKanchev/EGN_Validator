6e20e8ac9d63bc49790eca87d97f45a8cb80cc46
yotkoKanchev <yotko.kanchev@gmail.com>
2019-12-28 23:54:02 +0200

    Added symbolsValidator

--- EGN_Validator/Validators/SymbolsValidator.cs
+++ EGN_Validator/Validators/SymbolsValidator.cs
@@ -1,10 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace EGN_Validator.Validators
+﻿namespace EGN_Validator.Validators
 {
-    class SymbolsValidator
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class SymbolsValidator : Validator
     {
+        public override void Validate(string input)
+        {
+            var match = Regex.Match(input, @"^[0-9]{10}$");
+
+            if (!match.Success)  /*!input.All(char.IsDigit) */
+            {
+                throw new ArgumentException("EGN трябва да съдържа само неотрицателни цифри!");
+            }
+        }
     }
 }
