f10b8ec7918650072f93931f717a761eeaeb742b
yotkoKanchev <yotko.kanchev@gmail.com>
2019-12-28 23:55:05 +0200

    Added lenthValidator

--- EGN_Validator/Validators/LengthValidator.cs
+++ EGN_Validator/Validators/LengthValidator.cs
@@ -1,10 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace EGN_Validator.Validators
+﻿namespace EGN_Validator.Validators
 {
-    class LengthValidator
+    using System;
+
+    public class LengthValidator : Validator
     {
+        public override void Validate(string input)
+        {
+            if (input.Length != 10)
+            {
+                throw new ArgumentException("Дължината на ЕГН трябва да е точно 10 символа!");
+            }
+        }
     }
 }
