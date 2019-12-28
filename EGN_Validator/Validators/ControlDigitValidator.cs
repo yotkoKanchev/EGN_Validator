9a313baeef3b3e70e3fd29247047be9d23edf941
yotkoKanchev <yotko.kanchev@gmail.com>
2019-12-28 23:48:57 +0200

    Added validating classes

---
+++ EGN_Validator/Validators/ControlDigitValidator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace EGN_Validator.Validators
+{
+    public class ControlDigitValidator : Validator
+    {
+        public override void Validate(string input)
+        {
+            var controlDigit = int.Parse(input[^1].ToString());
+            var calculatedResult = CalculateResult(input[0..8]);
+
+            if (controlDigit != calculatedResult)
+            {
+                throw new ArgumentException("Последната цифра от ЕГН не е валидна!");
+            }
+        }
+
+        private int CalculateResult(string input)
+        {
+            var EGNWeights = new int[] { 2, 4, 8, 5, 10, 9, 7, 3, 6 };
+            var result = 0;
+
+            for (int i = 0; i < 9; i++)
+            {
+                result += int.Parse(input[i].ToString()) * EGNWeights[i];
+            }
+
+            result -= ((result / 11) * 11);
+            return result == 10 ? 0 : result;
+        }
+    }
+}
---
+++ EGN_Validator/Validators/LengthValidator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EGN_Validator.Validators
+{
+    class LengthValidator
+    {
+    }
+}
